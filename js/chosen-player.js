6dbf0f6e27ed533f76402b069462959c105f1c3a
jahidhasan8 <jahidhasan1932@gmail.com>
2022-08-22 10:25:21 +0600

    player choosing list added

--- js/chosen-player.js
+++ js/chosen-player.js
@@ -1,1 +1,31 @@
 
+
+const playerList = [];
+function choosePlayer(name) {
+      
+    // player choosing validation
+    if(playerList.length > 4){
+        return alert ('Sorry you can not choose more than 5 players')
+        }
+
+        // get player name
+    const playerName = name.parentNode.children[0].innerText
+    playerList.push(playerName);
+
+        // get choosing-player-list field 
+    const player = document.getElementById('choose-player-list');
+         player.innerText = '';
+     
+
+    for (let i = 0; i < playerList.length; i++) {
+
+        let chosenPlayerName = playerList[i];
+        const li = document.createElement('li');
+        
+        li.innerText=chosenPlayerName;
+       
+        player.appendChild(li);
+        
+    }
+
+}
